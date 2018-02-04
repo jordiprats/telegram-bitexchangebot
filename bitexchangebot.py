d711c869b77dc4645ddbbd6de02d3f211e35953b
jordiprats <jordi.prats@gmail.com>
2018-02-04 23:31:30 +0100

    info EMA i MACD

--- bitexchangebot.py
+++ bitexchangebot.py
@@ -9,3 +9,18 @@ BOT_TOKEN = config.get('bot', 'token')
 updater = Updater(token=BOT_TOKEN)
 
 #https://blockchain.info/ticker
+
+
+# MACD = EMA12 – EMA26
+
+# La fórmula:
+#
+# EMA(t) = EMA(t – 1) + K*[Precio(t) – EMA(t – 1)]
+#
+# Donde
+#
+# t=valor actual
+#
+# t-1=valor previo
+#
+# K= 2 / (n + 1) (n=periodo elegido para EMA)
